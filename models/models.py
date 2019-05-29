68073010e0749b3e07ec538a96714bb4cd7218bd
Yu Liangyong <yuly16@mails.tsinghua.edu.cn>
2019-05-29 11:29:35 +0800

    using conv models

--- models/models.py
+++ models/models.py
@@ -1,42 +1,75 @@
+'''
+@project: Speaker verification SDK
+@author: Yutian Li
+@file: model_xvector.py
+@time: 2019-4-24
+'''
+
 import torch
-from torch import nn
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import Parameter
+from torch.autograd import Variable
 from torch.nn.functional import softmax
+
 class X_vector(nn.Module):
-	def __init__(self):
-		super(X_vector,self).__init__()
-		node_num = [256,512,1024,512,256]
-		self.TDNN = nn.Sequential(TDNN(20,node_num[0],5,1),
-			TDNN(node_num[0],node_num[1],3,2),
-			TDNN(node_num[1],node_num[2],3,3),)
-		self.sts_pooling = statistic_pooling()
-		self.fc1 = nn.Sequential(fc(node_num[2]*2,node_num[3]),fc(node_num[3],node_num[4]))
-		self.fc2 = nn.Linear(node_num[4], 2)
-	def forward(self,x):
-		x = self.TDNN(x)
-		x = self.sts_pooling(x)
-		x = self.fc1(x)
-		x = softmax(self.fc2(x))
-		return x
+    def __init__(self):
+        super(X_vector, self).__init__()
 
-class TDNN(nn.Module):
-	def __init__(self,input_dim,output_dim,kernal_size,dilation):
-		super(TDNN,self).__init__()
-		self.TDNN = nn.Sequential(nn.Conv1d(input_dim, output_dim, kernal_size, stride=1, dilation=dilation),
-			nn.BatchNorm1d(output_dim),nn.ReLU())
-	def forward(self,x):
-		return self.TDNN(x)
-class fc(nn.Module):
-	def __init__(self,input_dim,output_dim):
-		super(fc,self).__init__()
-		self.fc = nn.Sequential(nn.Linear(input_dim, output_dim),nn.BatchNorm1d(output_dim),nn.ReLU())
-	def forward(self,x):
-		return self.fc(x)
-class statistic_pooling(nn.Module):
-	def __init__(self):
-		super(statistic_pooling,self).__init__()
-	def forward(self,x):
-		mean_x = x.mean(dim = 2)
-		std_x = x.std(dim = 2)
-		mean_std = torch.cat((mean_x, std_x), 1)
-		return mean_std
+        self.tdnn1 = nn.Conv2d(1, 512, (5,20))
+        self.bn1 = nn.BatchNorm1d(512)
 
+        self.tdnn2 = nn.Conv2d(1, 512, (5,512))
+        self.bn2 = nn.BatchNorm1d(512)
+
+        self.tdnn3 = nn.Conv2d(1, 512, (7,512))
+        self.bn3 = nn.BatchNorm1d(512)
+ 
+        self.fc1= nn.Sequential(nn.Linear(1024, 1024),nn.BatchNorm1d(1024),nn.ReLU())
+        self.fc2= nn.Sequential(nn.Linear(1024, 512),nn.BatchNorm1d(512),nn.ReLU())
+        self.fc3= nn.Linear(512, 2)
+
+
+
+
+
+    def forward(self, x):
+
+        #frame level tdnn
+
+        x_shape=x.data.shape
+        x=x.view(x_shape[0],1,x_shape[1],x_shape[2])
+        x = self.tdnn1(x)
+        x_shape=x.data.shape
+        x=x.view(x_shape[0],x_shape[1],x_shape[2])
+        x = F.relu(self.bn1(x))
+        x = x.transpose(1, 2).contiguous()
+
+        x_shape=x.data.shape
+        x=x.view(x_shape[0],1,x_shape[1],x_shape[2])
+        x = self.tdnn2(x)
+        x_shape=x.data.shape
+        x=x.view(x_shape[0],x_shape[1],x_shape[2])
+        x = F.relu(self.bn2(x))
+        x = x.transpose(1, 2).contiguous()
+
+        x_shape=x.data.shape
+        x=x.view(x_shape[0],1,x_shape[1],x_shape[2])
+        x = self.tdnn3(x)
+        x_shape=x.data.shape
+        x=x.view(x_shape[0],x_shape[1],x_shape[2])
+        x = F.relu(self.bn3(x))
+        x = x.transpose(1, 2).contiguous()
+
+
+
+        mean = torch.mean(x,1)
+        std=torch.std(x,1)
+        x = torch.cat((mean,std),1)
+
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = softmax(self.fc3(x))
+
+
+        return x
